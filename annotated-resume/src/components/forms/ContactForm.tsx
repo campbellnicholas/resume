fecb4d06294b9260dec233e3f63dd8983ffd5606
Nick Campbell <campbell.nicholas@gmail.com>
2025-04-16 15:37:32 -0700

    Update form to both work and be secret

--- annotated-resume/src/components/forms/ContactForm.tsx
+++ annotated-resume/src/components/forms/ContactForm.tsx
@@ -130,7 +130,7 @@ const ContactFormComponent = ({ isOpen, onClose }: ContactFormProps): React.Reac
     setStatus('submitting');
     
     try {
-      const response = await fetch('https://formspree.io/f/your-form-id', {
+      const response = await fetch(`https://formspree.io/f/${process.env.VITE_FORMSPREE_FORM_ID}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
