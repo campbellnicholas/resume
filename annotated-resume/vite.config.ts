b230a7047b9761d2f845b54abb29ef2221e0ca12
Nick Campbell <campbell.nicholas@gmail.com>
2025-04-15 17:28:22 -0700

    Fix: Configure Vite to use relative paths for assets

--- annotated-resume/vite.config.ts
+++ annotated-resume/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for all assets
   test: {
     globals: true,
     environment: 'jsdom',
